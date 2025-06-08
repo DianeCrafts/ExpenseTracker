9b861cdfc009d9aba758ae96e4cc41751158a165
DianCrafts <mahdieh.ghorbaaniaan@gmail.com>
2025-06-08 01:15:11 -0400

    made the code cleaner

--- backend/src/main/java/com/example/expensetracker/model/Expense.java
+++ backend/src/main/java/com/example/expensetracker/model/Expense.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import java.time.LocalDate;
+
 
 @Entity
 public class Expense {
